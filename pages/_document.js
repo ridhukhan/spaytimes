b508078ecb8eed075fa40bcd6379ad3f5ffc219d
ridhukhan <riyadkhan6598@gmail.com>
2026-06-05 16:18:49 +0600

    update

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="monetag" content="55b3c8eebef00e9bf614d7959d49b527"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
