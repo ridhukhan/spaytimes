1095e7e2d876fab0ffff6878d6e82bb360fe9449
ridhukhan <riyadkhan6598@gmail.com>
2026-06-05 16:29:15 +0600

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="monetag" content="55b3c8eebef00e9bf614d7959d49b527"></meta>
+        {/* Monetag ভেরিফিকেশন মেটা ট্যাগ */}
+        <meta name="monetag" content="55b3c8eebef00e9bf614d7959d49b527" />
+
+        {/* Monetag বিজ্ঞাপনের মেইন স্ক্রিপ্ট */}
+        <Script
+          src="https://quge5.com/88/tag.min.js"
+          data-zone="246429"
+          strategy="afterInteractive"
+          data-cfasync="false"
+        />
       </Head>
       <body>
         <Main />
