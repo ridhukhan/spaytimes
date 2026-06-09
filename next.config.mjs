7375c8cc38d0ac12e6ddc43d0d149741ba25514f
ridhukhan <riyadkhan6598@gmail.com>
2026-06-09 21:19:39 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ["bn", "en", "hi", "ta", "zh", "es", "pt", "tr", "de", "fr", "ar"],
-    defaultLocale: "bn",
-    localeDetection: true,
+    defaultLocale: "bn", // বাংলাদেশের জন্য ডিফল্ট বাংলা
+    localeDetection: true, // আইপি/দেশ অনুযায়ী অটো ল্যাঙ্গুয়েজ ডিটেকশন
   },
 };
 
-module.exports = nextConfig;
+// 🎯 module.exports এর পরিবর্তে ES Module এ export default লিখতে হয়
+export default nextConfig;
